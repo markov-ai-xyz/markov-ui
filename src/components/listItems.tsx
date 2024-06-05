f029b3df5c92bbfe9ffcc0a941138758ae432ab1
Anant Chandra <anantcha@usc.edu>
2024-06-05 14:54:30 +0530

    Update icons on portal

--- src/components/listItems.tsx
+++ src/components/listItems.tsx
@@ -4,8 +4,8 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import ListSubheader from '@mui/material/ListSubheader';
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import PeopleIcon from '@mui/icons-material/People';
+import ArchiveSharpIcon from '@mui/icons-material/ArchiveSharp';
+import UnarchiveSharpIcon from '@mui/icons-material/UnarchiveSharp';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import LayersIcon from '@mui/icons-material/Layers';
 import AssignmentIcon from '@mui/icons-material/Assignment';
@@ -20,13 +20,13 @@ export const mainListItems = (
     </ListItemButton>
     <ListItemButton>
       <ListItemIcon>
-        <ShoppingCartIcon />
+        <ArchiveSharpIcon />
       </ListItemIcon>
       <ListItemText primary="Inbox" />
     </ListItemButton>
     <ListItemButton>
       <ListItemIcon>
-        <PeopleIcon />
+        <UnarchiveSharpIcon />
       </ListItemIcon>
       <ListItemText primary="Outbound messaging" />
     </ListItemButton>
